3e02e117cc8dc7642c763a4033e5ad51747d55a7
catkitty <mateusz.karcz@interia.eu>
2017-06-28 10:57:14 +0200

    Automatyzacja zarządzania pamięcią podręczną.

---
+++ plugins/Cacher/functions.php
@@ -0,0 +1,55 @@
+<?php
+  namespace Plugin\Cacher;
+
+  function get_filename($pagename, $language, $theme) {
+    global $ws;
+    return $ws['PATH_CACHE'] . str_replace('/', '_', $pagename) . '$' . $language . '$' . $theme . '.html';
+  }
+  
+  function cached($pagename, $language, $theme) {
+    global $ws;
+    $filename = get_filename($pagename, $language, $theme);
+    if(!file_exists($filename)) return false;
+    return true;
+  }
+
+  function stale($pagename, $language, $theme) {
+    global $ws;
+    $filename = get_filename($pagename, $language, $theme);
+    if(!file_exists($filename)) return false;
+    
+    $pagepath = $ws['PATH_CONTENT'] . $pagename;
+    if(!file_exists($pagepath)) return true;
+    if(!is_dir($pagepath)) return true;
+    
+    $dir = opendir($pagepath);
+    while($sect = readdir($dir))
+      if(preg_match('/^(\d{3})-([\w-]+).html$/', $sect))
+        if(filemtime($filename) < filemtime($pagepath . '/' . $sect))
+          return true;
+    
+    return false;
+  }
+  
+  function get($pagename, $language, $theme) {
+    if(cached($pagename, $language, $theme))
+      return file_get_contents(get_filename($pagename, $language, $theme));
+    return false;
+  }
+  
+  function put($pagename, $language, $theme, $doc) {
+    global $ws;
+    
+    $filename = get_filename($pagename, $language, $theme);
+    if(file_exists($filename)) unlink($filename);
+    $doc .= "\r\n" . '<!-- ' . $ws['lang']['cached'];
+    $doc .= date(' j ') . $ws['lang']['months']['genitive'][date('n')-1] . date(' o, H:i:s T');
+    $doc .= '. -->';
+    file_put_contents($filename, $doc);
+  }
+
+  function clear($pagename, $language, $theme) {
+    if(cached($pagename, $language, $theme))
+      unlink(get_filename($pagename, $language, $theme));
+  }
+?>
--- plugins/Cacher/plugin.php
+++ plugins/Cacher/plugin.php
@@ -1,4 +1,6 @@
 <?php
+  require_once('functions.php');
+  
   $pl_initialize = function() {
     global $ws;
 
@@ -9,8 +11,13 @@
     global $ws;
     global $content;
 
-    if($ws['Cache'] && cacher_cached($uri, $ws['lang']['code'], $ws['Theme'])) {
-      $content = cacher_get($uri, $ws['lang']['code'], $ws['Theme']);
+    if($ws['Cache'] && Plugin\Cacher\cached($ws['Page'], $ws['lang']['code'], $ws['Theme'])) {
+      if(Plugin\Cacher\stale($ws['Page'], $ws['lang']['code'], $ws['Theme'])) {
+        Plugin\Cacher\clear($ws['Page'], $ws['lang']['code'], $ws['Theme']);
+        return false;
+      }
+      
+      $content = Plugin\Cacher\get($ws['Page'], $ws['lang']['code'], $ws['Theme']);
       return true;
     }
     return false;
@@ -20,38 +27,7 @@
     global $ws;
     global $content;
 
-    if($ws['Page'] != 'error' && $ws['Cache'])
-      cacher_put($ws['Page'], $ws['lang']['code'], $ws['Theme'], $content);
+    if($ws['Page'] != 'error' && $ws['Cache'] && !Plugin\Cacher\cached($ws['Page'], $ws['lang']['code'], $ws['Theme']))
+      Plugin\Cacher\put($ws['Page'], $ws['lang']['code'], $ws['Theme'], $content);
   };
-
-
-  function cacher_get_filename($pagename, $language, $theme) {
-    global $ws;
-    return $ws['PATH_CACHE'] . str_replace('/', '_', $pagename) . '$' . $language . '$' . $theme . '.html';
-  }
-  
-  function cacher_cached($pagename, $language, $theme) {
-    global $ws;
-    $filename = cacher_get_filename($pagename, $language, $theme);
-    if(!file_exists($filename)) return false;
-    if(time() - $ws['CacheTime'] > filemtime($filename)) return false;
-    return true;
-  }
-  
-  function cacher_get($pagename, $language, $theme) {
-    if(cacher_cached($pagename, $language, $theme))
-      return file_get_contents(cacher_get_filename($pagename, $language, $theme));
-    return false;
-  }
-  
-  function cacher_put($pagename, $language, $theme, $doc) {
-    global $ws;
-    
-    $filename = cacher_get_filename($pagename, $language, $theme);
-    if(file_exists($filename)) unlink($filename);
-    $doc .= "\r\n" . '<!-- ' . $ws['lang']['cached'];
-    $doc .= date(' j ') . $ws['lang']['months']['genitive'][date('n')-1] . date(' o, H:i:s T');
-    $doc .= '. -->';
-    file_put_contents($filename, $doc);
-  }
 ?>
