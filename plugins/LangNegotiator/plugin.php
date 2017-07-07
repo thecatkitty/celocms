59148873d032cc918133bd155b6d3b0b68621c67
catkitty <mateusz.karcz@interia.eu>
2017-07-07 13:42:33 +0200

    Nie dało się wymusić języka. ;-;

--- plugins/LangNegotiator/plugin.php
+++ plugins/LangNegotiator/plugin.php
@@ -6,8 +6,7 @@
     if(Session\is_forced('lang'))
       if(Localizer\language_available(Session\get_forced('lang')))
         $ws['Language'] = Session\get_forced('lang');
-
-    if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    else if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
       $acc_langs = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
       foreach($acc_langs as $al) {
         $al = preg_replace('/^([a-z]{2,3})(-[a-zA-Z]+)*$/', '$1', $al);
