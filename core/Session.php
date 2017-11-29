1003a4d37388a13f29f7279e2e3c626352071f7e
Mateusz Karcz <mateusz.karcz@interia.eu>
2017-11-29 22:54:07 +0100

    IE7-8 oficjalnie archaiczne.

--- core/Session.php
+++ core/Session.php
@@ -16,7 +16,10 @@
     if($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.0')
       return true;
 
-    if(!preg_match("/Trident|Gecko|Opera\/9|bot|spider|crawl|slurp|facebook/", $_SERVER['HTTP_USER_AGENT']))
+    if(preg_match('/MSIE [1-8]\.0/'))
+      return true;
+
+    if(!preg_match('/Trident|Gecko|Opera\/9|bot|spider|crawl|slurp|facebook/', $_SERVER['HTTP_USER_AGENT']))
       return true;
 
     return false;
