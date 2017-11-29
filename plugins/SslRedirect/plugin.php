7b769f56ac2f1d096e43ab9c7bed12779f9b27f4
Mateusz Karcz <mateusz.karcz@interia.eu>
2017-11-29 23:06:01 +0100

    IE9-10 domyślnie nie obsługują TLS1.1

--- plugins/SslRedirect/plugin.php
+++ plugins/SslRedirect/plugin.php
@@ -2,7 +2,7 @@
   $pl_initialize = function() {
     // Przekieruj na połączenie szyfrowane
     if($_SERVER['HTTP_HOST'] == 'www.celones.pl')
-      if(!Session\archaic_agent()) {
+      if(!Session\archaic_agent() || preg_match('/MSIE (9|10)/', $_SERVER['HTTP_USER_AGENT'])) {
         header("Location: https://celones.pl" . $_SERVER['REQUEST_URI'], true, 301);
         exit();
       }
