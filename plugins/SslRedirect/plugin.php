d6215205017240561ad902d8cbb0bb21257e425d
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-02-05 12:42:35 +0100

    SslRedirect: Do not assume the host name

--- plugins/SslRedirect/plugin.php
+++ plugins/SslRedirect/plugin.php
@@ -1,9 +1,9 @@
 <?php
 $pl_initialize = function () {
   // Przekieruj na połączenie szyfrowane
-  if ($_SERVER['HTTP_HOST'] == 'www.celones.pl')
+  if (substr($_SERVER['HTTP_HOST'], 0, 4) == 'www.')
     if (!Session\archaic_agent() || preg_match('/MSIE (9|10)/', $_SERVER['HTTP_USER_AGENT'])) {
-      header("Location: https://celones.pl" . $_SERVER['REQUEST_URI'], true, 301);
+      header("Location: https://" . substr($_SERVER['HTTP_HOST'], 4) . $_SERVER['REQUEST_URI'], true, 301);
       exit();
     }
 };
