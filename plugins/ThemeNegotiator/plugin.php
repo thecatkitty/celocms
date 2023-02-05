3487db82368b63a7cb5b268178f323294d6a61b7
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-02-05 11:53:51 +0100

    Theme: Allow external hosting of themes

--- plugins/ThemeNegotiator/plugin.php
+++ plugins/ThemeNegotiator/plugin.php
@@ -5,11 +5,14 @@ $pl_initialize = function () {
   // Negocjuj motyw
   $ws['Theme'] = Session\get_forced('theme', Session\archaic_agent() ? 'archaic' : $ws['Theme']);
   $ws['ThemePath'] = $ws['PATH_THEME'] . $ws['Theme'] . '/';
-
   if (!file_exists($ws['ThemePath'] . 'theme.php')) {
     $ws['Theme'] = 'archaic';
     $ws['ThemePath'] = $ws['PATH_THEME'] . $ws['Theme'] . '/';
     $_SESSION['theme'] = 'archaic';
+  }
+
+  if ($ws['URL_THEME'][0] != '/') {
+    $ws['URL_THEME'] = $ws['PATH_ROOT'] . $ws['URL_THEME'];
   }
 };
 ?>
